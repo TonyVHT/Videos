a08c7a3caa0822793019b3300f3935dc57c7da7c
TonyVHT <tvillegashu@gmail.com>
2024-07-04 21:40:45 -0600

    curso

---
+++ Video2/java/DataType.java
@@ -0,0 +1,32 @@
+public class DataType {
+    public static void main(String[] args) {
+
+        //tipo de dato primitivo y los valores que pueden almacenar
+
+        System.out.println("\nTipos de datos primitivos y los valores que pueden almacenar\n"); 
+
+        byte a = 0; //almacena valores de -128 a 127
+
+        short b = 0; //almacena valores de -32768 a 32767
+
+        int c = 0; //almacena valores de -2147483648 a 2147483647
+
+        long d = 0; //almacena valores de -9223372036854775808 a 9223372036854775807
+
+        float e = 0f; //almacena valores de 1.4e-045 a 3.4e+038
+
+        double f = 0; //almacena valores de 4.9e-324 a 1.8e+308
+
+        char g = 32; //almacena valores de 0 a 65535
+
+        System.out.println(g);
+
+        boolean h = true; //almacena valores de true o false
+
+        //tipo de dato no primitivo
+        String i = "Hola mundo"; //String es un tipo de dato no primitivo que almacena cadenas de texto
+    }
+}
+
+    /* tabla de valores ASCII
+    https://elcodigoascii.com.ar/ */
---
+++ Video2/java/GravityCalculator.java
@@ -0,0 +1,5 @@
+public class GravityCalculator {
+    public static void main(String[] arguments) {
+
+    }
+}
